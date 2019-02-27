725623cdc891e8c92c931432faddbcd7e614d407
sunjianqin <jianqin.sun@daoyitong.com>
2019-02-27 11:04:24 +0800

    提交文件

---
+++ JQPodsAPP/JQPodsAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JQPodsAPP
+//
+//  Created by maoge on 2019/2/27.
+//  Copyright © 2019 maoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JQPodsAPP/JQPodsAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JQPodsAPP
+//
+//  Created by maoge on 2019/2/27.
+//  Copyright © 2019 maoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
